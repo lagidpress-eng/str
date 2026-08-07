773f0d41006997572c27456e2766e217815bff2b
lagidpress-eng <lagid.press@gmail.com>
2026-08-07 00:35:45 -0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-
-const CACHE='mr-agent-v2';
-const FILES=['./','./index.html','./styles.css','./app.js','./data.json','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
